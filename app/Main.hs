36d222f2d1210946e8dd44c82905698e74b33564
schromercy <armandarius.darmadji@gmail.com>
2022-06-25 15:13:28 +0800

    Use WriterT to accumulate logs while creating ball

--- app/Main.hs
+++ app/Main.hs
@@ -5,55 +5,26 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Simulate
 import Data.Time
 import Text.Read
+import Control.Monad.Trans.Writer ( WriterT, tell, runWriterT )
+import Control.Monad.IO.Class ( liftIO )
 
 main :: IO ()
 main = do
-    writeLineLog "Bouncing ball initialized..."
+        writeLineLog "Bouncing ball initialized..."
 
-    -- Initialize values.
-    putStrLn "Input bound x (100 - 500): "
-    boundXStr <- getLine
-    let boundX = clamp 100 500 (read boundXStr :: Float)
-    appendLineLog $ "Bound X is " ++ show boundX
+        (ball, logs) <- runWriterT createBall
+        appendLogs logs
 
-    putStrLn "Input bound y (100 - 500): "
-    boundYStr <- getLine
-    let boundY = clamp 100 500 (read boundYStr :: Float)
-    appendLineLog $ "Bound Y is " ++ show boundY
+        -- Start simulation
+        appendLineLog "Starting simulation..."
+        simulateIO (window "Bouncing Ball" (getBound ball)) black 60 ball drawBall (const updateBall)
+    where
+        getBound :: Ball -> Point
+        getBound (_, _, _, _, _, bound, _) = bound
 
-    putStrLn "Input ball size (10 - 50): "
-    ballRadiusStr <- getLine
-    let ballRadius = clamp 10 50 (read ballRadiusStr :: Float)
-    appendLineLog $ "Ball radius is " ++ show ballRadius
-
-    putStrLn "Input initial ball speed (50 - 100): "
-    initVelocityStr <- getLine
-    let initVelocity = clamp 50 100 (read initVelocityStr :: Float)
-    let ballInitVelocity = (initVelocity, initVelocity)
-    appendLineLog $ "Ball initial velocity is " ++ show initVelocity
-
-    putStrLn "Input velocity increment (0 - 100): "
-    incVelocityStr <- getLine
-    let incVelocity = clamp 0 100 (read incVelocityStr :: Float)
-    let ballIncVelocity = (incVelocity, incVelocity)
-    appendLineLog $ "Ball incremental velocity is " ++ show incVelocity
-
-    putStrLn "Input maximal velocity (50 - 1000): "
-    maxVelocityStr <- getLine
-    let maxVelocity = clamp 100 1000 (read maxVelocityStr :: Float)
-    let ballMaxVelocity = (maxVelocity, maxVelocity)
-    appendLineLog $ "Ball maximal velocity is " ++ show maxVelocity
-
-    x <- getStdRandom (randomR (-boundX, boundX))
-    y <- getStdRandom (randomR (-boundY, boundY))
-    appendLineLog $ "Ball random position is (" ++ show x ++ ", " ++ show y ++ ")"
-
-    let bound = (boundX, boundY)
-    let ball = ((x, y), ballRadius, ballInitVelocity, ballIncVelocity, ballMaxVelocity, bound, [white, red, green, blue])
-
-    -- Start simulation
-    appendLineLog "Starting simulation..."
-    simulateIO (window "Bouncing Ball" bound) black 60 ball drawBall (const updateBall)
+        appendLogs :: [String] -> IO ()
+        appendLogs (x:xs) = appendLineLog x >> appendLogs xs
+        appendLogs [] = return ()
 
 -- Ball type
 --      Position
@@ -100,6 +71,71 @@ updateBall dt ((x, y), radius, (dx, dy), (incDx, incDy), (maxDx, maxDy), (boundX
                     appendLineLog $ "Ball new color is " ++ show (head newColors)
                     return newColors
                 | otherwise = return colors
+
+createBall :: WriterT [String] IO Ball
+createBall = do
+    liftIO $ putStrLn "Input bound X (100 - 400, def: 300): "
+    unclampBoundX <- liftIO $ safeGetLineFloat 300
+    let boundX = clamp 100 400 unclampBoundX
+    let boundXLog = "Bound X is " ++ show boundX
+    liftIO $ putStrLn boundXLog
+    tell [boundXLog]
+
+    liftIO $ putStrLn "Input bound Y (100 - 400, def: 300): "
+    unclampBoundY <- liftIO $ safeGetLineFloat 300
+    let boundY = clamp 100 400 unclampBoundY
+    let boundYLog = "Bound Y is " ++ show boundY
+    liftIO $ putStrLn boundYLog
+    tell [boundYLog]
+
+    liftIO $ putStrLn "Input ball size (10 - 50, def: 25): "
+    unclampBallRadius <- liftIO $ safeGetLineFloat 25
+    let ballRadius = clamp 10 50 unclampBallRadius
+    let ballRadiusLog = "Ball radius is " ++ show ballRadius
+    liftIO $ putStrLn ballRadiusLog
+    tell [ballRadiusLog]
+
+    liftIO $ putStrLn "Input initial ball speed (50 - 100, def: 75): "
+    unclampInitVelocity <- liftIO $ safeGetLineFloat 75
+    let initVelocity = clamp 50 100 unclampInitVelocity
+    let ballInitVelocity = (initVelocity, initVelocity)
+    let ballInitVelocityLog = "Ball initial velocity is " ++ show initVelocity
+    liftIO $ putStrLn ballInitVelocityLog
+    tell [ballInitVelocityLog]
+
+    liftIO $ putStrLn "Input velocity increment (0 - 100, def: 50): "
+    unclampIncVelocity <- liftIO $ safeGetLineFloat 50
+    let incVelocity = clamp 0 100 unclampIncVelocity
+    let ballIncVelocity = (incVelocity, incVelocity)
+    let incVelocityLog = "Ball incremental velocity is " ++ show incVelocity
+    liftIO $ putStrLn incVelocityLog
+    tell [incVelocityLog]
+
+    liftIO $ putStrLn "Input maximal velocity (50 - 2000, def: 1000): "
+    unclampMaxVelocity <- liftIO $ safeGetLineFloat 1000
+    let maxVelocity = clamp 50 2000 unclampMaxVelocity
+    let ballMaxVelocity = (maxVelocity, maxVelocity)
+    let maxVelocityLog = "Ball maximal velocity is " ++ show maxVelocity
+    liftIO $ putStrLn maxVelocityLog
+    tell [maxVelocityLog]
+
+    x <- getStdRandom (randomR (-boundX, boundX))
+    y <- getStdRandom (randomR (-boundY, boundY))
+    let randomInitPosLog = "Ball random position is (" ++ show x ++ ", " ++ show y ++ ")"
+    liftIO $ putStrLn randomInitPosLog
+    tell [randomInitPosLog]
+
+    let bound = (boundX, boundY)
+    let ball = ((x, y), ballRadius, ballInitVelocity, ballIncVelocity, ballMaxVelocity, bound, [white, red, green, blue])
+    return ball
+
+safeGetLineFloat :: Float -> IO Float
+safeGetLineFloat defValue = do
+    value <- getLine
+    let maybeValue = readMaybe value :: Maybe Float
+    case maybeValue of
+        Nothing -> return defValue
+        Just x -> return x
 
 clamp :: (Ord a)
       => a
