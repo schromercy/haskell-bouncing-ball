423f70a5684c2a846d7a6e698039ff6249e57c10
Regulus <regulusstudio@gmail.com>
2022-06-25 12:02:47 +0800

    Add logging functionality

--- app/Main.hs
+++ app/Main.hs
@@ -2,42 +2,58 @@ module Main where
 
 import System.Random
 import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Simulate
+import Data.Time
+import Text.Read
 
 main :: IO ()
 main = do
+    writeLineLog "Bouncing ball initialized..."
+
+    -- Initialize values.
     putStrLn "Input bound x (100 - 500): "
     boundXStr <- getLine
     let boundX = clamp 100 500 (read boundXStr :: Float)
+    appendLineLog $ "Bound X is " ++ show boundX
 
     putStrLn "Input bound y (100 - 500): "
     boundYStr <- getLine
     let boundY = clamp 100 500 (read boundYStr :: Float)
+    appendLineLog $ "Bound Y is " ++ show boundY
 
     putStrLn "Input ball size (10 - 50): "
     ballRadiusStr <- getLine
     let ballRadius = clamp 10 50 (read ballRadiusStr :: Float)
+    appendLineLog $ "Ball radius is " ++ show ballRadius
 
     putStrLn "Input initial ball speed (50 - 100): "
     initVelocityStr <- getLine
     let initVelocity = clamp 50 100 (read initVelocityStr :: Float)
     let ballInitVelocity = (initVelocity, initVelocity)
+    appendLineLog $ "Ball initial velocity is " ++ show initVelocity
 
     putStrLn "Input velocity increment (0 - 100): "
     incVelocityStr <- getLine
     let incVelocity = clamp 0 100 (read incVelocityStr :: Float)
     let ballIncVelocity = (incVelocity, incVelocity)
+    appendLineLog $ "Ball incremental velocity is " ++ show incVelocity
 
     putStrLn "Input maximal velocity (50 - 1000): "
     maxVelocityStr <- getLine
     let maxVelocity = clamp 100 1000 (read maxVelocityStr :: Float)
     let ballMaxVelocity = (maxVelocity, maxVelocity)
+    appendLineLog $ "Ball maximal velocity is " ++ show maxVelocity
 
     x <- getStdRandom (randomR (-boundX, boundX))
     y <- getStdRandom (randomR (-boundY, boundY))
+    appendLineLog $ "Ball random position is (" ++ show x ++ ", " ++ show y ++ ")"
 
     let bound = (boundX, boundY)
     let ball = ((x, y), ballRadius, ballInitVelocity, ballIncVelocity, ballMaxVelocity, bound)
-    simulate (window "Bouncing Ball" bound) black 60 ball drawBall (const updateBall)
+
+    -- Start simulation
+    appendLineLog "Starting simulation..."
+    simulateIO (window "Bouncing Ball" bound) black 60 ball drawBall (const updateBall)
 
 -- Ball type
 --      Position
@@ -51,23 +67,29 @@ type Ball = (Point, Float, Vector, Vector, Vector, Point)
 window :: String -> Point -> Display
 window title (boundX, boundY) = InWindow title  (2 * floor boundX, 2 * floor boundY) (0, 0)
 
-drawBall :: Ball -> Picture
-drawBall ((x, y), radius, _, _, _, _) = translate x y $ color white $ circleSolid radius
+drawBall :: Ball -> IO Picture
+drawBall ((x, y), radius, _, _, _, _) = do
+    return (translate x y $ color white $ circleSolid radius)
 
-updateBall :: Float -> Ball -> Ball
-updateBall dt ((x, y), radius, (dx, dy), (incDx, incDy), (maxDx, maxDy), (boundX, boundY)) = ((x', y'), radius, (dx', dy'), (incDx, incDy), (maxDx, maxDy), (boundX, boundY))
-    where
-        (x', dx') = clip x (boundX - radius) dx incDx maxDx
-        (y', dy') = clip y (boundY - radius) dy incDy maxDy
-
-        -- clip to a bounding interval
-        clip h max dh incDh maxDh
-            | h' > max      = ( max, -absDh')
-            | h' < -max     = (-max, absDh')
-            | otherwise     = (h', dh)
-            where
-                h'      = h + dt * dh
-                absDh'  = min (abs dh + incDh) maxDh
+updateBall :: Float -> Ball -> IO Ball
+updateBall dt ((x, y), radius, (dx, dy), (incDx, incDy), (maxDx, maxDy), (boundX, boundY)) = do
+        (x', dx') <- clip "X" x (boundX - radius) dx incDx maxDx
+        (y', dy') <- clip "Y" y (boundY - radius) dy incDy maxDy
+        return ((x', y'), radius, (dx', dy'), (incDx, incDy), (maxDx, maxDy), (boundX, boundY))
+        where
+            -- clip to a bounding interval
+            clip axis h max dh incDh maxDh
+                | h' > max  = do
+                    appendLineLog $ "Bounce, " ++ axis ++ "-speed now: " ++ show absDh'
+                    return ( max, -absDh')
+                | h' < -max = do
+                    appendLineLog $ "Bounce, " ++ axis ++ "-speed now: " ++ show absDh'
+                    return (-max, absDh')
+                | otherwise = do
+                    return (h', dh)
+                where
+                    h'      = h + dt * dh
+                    absDh'  = min (abs dh + incDh) maxDh
 
 clamp :: (Ord a)
       => a
@@ -79,3 +101,22 @@ clamp :: (Ord a)
       -> a
 clamp mn mx val = max mn (min val mx)
 
+logFileName :: String
+logFileName = "log.txt"
+
+writeLineLog :: String -> IO ()
+writeLineLog message = do
+    formattedMessage <- formatLogMessage message
+    let finalMessage = formattedMessage ++ "\n"
+    writeFile logFileName finalMessage
+
+appendLineLog :: String -> IO ()
+appendLineLog message = do
+    formattedMessage <- formatLogMessage message
+    let finalMessage = formattedMessage ++ "\n"
+    appendFile logFileName finalMessage
+
+formatLogMessage :: String -> IO String
+formatLogMessage message = do
+    zonedTime <- getZonedTime
+    return $ show zonedTime ++ ": " ++ message
